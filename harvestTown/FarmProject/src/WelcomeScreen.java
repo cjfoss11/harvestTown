5cb0d163286378a5e131f0eea237d4f44a283518
cjfoss11 <cjfoss2013@gmail.com>
2020-11-30 19:53:14 -0500

    completed UI for WelcomeScreen

--- harvestTown/FarmProject/src/WelcomeScreen.java
+++ harvestTown/FarmProject/src/WelcomeScreen.java
@@ -6,12 +6,14 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class WelcomeScreen {
     private static final int SCREEN_WIDTH = 1000;
     private static final int SCREEN_HEIGHT = 800;
     private static final BorderPane ROOT = new BorderPane();
+    private static final StackPane STACK = new StackPane();
     private static final Scene SCENE = new Scene(ROOT, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     public static Scene getScene() { return SCENE; }
@@ -22,22 +24,33 @@ public class WelcomeScreen {
         welcomeVBox.setPadding(new Insets(10, 50, 50, 50));
         welcomeVBox.setAlignment(Pos.BASELINE_CENTER);
         Button startButton = new Button("Let's start harvesting!");
-        startButton.setTextFill(Color.web("9EB25D"));
+        startButton.setTextFill(Color.web("311E10"));
 
-        startButton.setBackground(new Background(new BackgroundFill(Color.web("F1DB4B"), CornerRadii.EMPTY, Insets.EMPTY)));
-        startButton.setMaxSize(300, 600);
+        startButton.setBackground(new Background(new BackgroundFill(Color.web("D9DD92"), CornerRadii.EMPTY, Insets.EMPTY)));
+        startButton.setMaxSize(400, 600);
+        startButton.setPadding((new Insets(30, 30, 30, 30)));
         startButton.setId("startButton");
-        welcomeVBox.getChildren().add(startButton);
 
         //Adding welcome text
         Text welcomeText = new Text("Welcome to Harvest Town!");
-        welcomeText.setFont(Font.font("Georgia",60));
-        welcomeText.setFill(Color.web("9EB25D"));
+        welcomeText.setTextAlignment(TextAlignment.CENTER);
+        welcomeText.setFont(Font.font("Cooper Black",60));
+        welcomeText.setFill(Color.web("DD6031"));
+        VBox vBox = new VBox();
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(30, 30, 30, 30));
+        vBox.getChildren().add(welcomeText);
+        vBox.getChildren().add(hBox);
+        vBox.getChildren().add(startButton);
+        vBox.setAlignment(Pos.CENTER);
+        STACK.setBackground(new Background(new BackgroundFill(Color.web("EABE7C"), CornerRadii.EMPTY, Insets.EMPTY)));
+        STACK.getChildren().add(vBox);
+        STACK.setMaxSize(1000, 500);
 
         //Config Welcome screen
         ROOT.setBottom(welcomeVBox);
-        ROOT.setCenter(welcomeText);
-        ROOT.setBackground(new Background(new BackgroundFill(Color.rgb(238, 252,206), CornerRadii.EMPTY, Insets.EMPTY)));
+        ROOT.setCenter(STACK);
+        ROOT.setBackground(new Background(new BackgroundFill(Color.web("ECE4B7"), CornerRadii.EMPTY, Insets.EMPTY)));
 
         stage.setScene(SCENE);
 
